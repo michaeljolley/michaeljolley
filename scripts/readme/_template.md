0349abf5ea441166d40d43209bbfe4e042ff5071
Michael Jolley <mike@sparcapp.io>
2021-08-13 20:49:19 -0500

    Adding GH action

---
+++ scripts/readme/_template.md
@@ -0,0 +1,21 @@
+## Hi there 👋
+
+### Recent Blog Posts
+
+{{#posts}}
+[{{title}}]({{link}})
+
+{{/posts}}
+### Recent Videos
+
+<table>
+  <tr>
+    {{#videos}}
+    <td>
+      <a href="{{link}}">
+        <img src="{{thumbnail}}" alt="{{title}}"/>
+      </a>
+    </td>
+    {{/videos}}
+  </tr>
+</table>
---
+++ scripts/readme/index.js
@@ -0,0 +1,94 @@
+const Parser = require('rss-parser');
+const Mustache = require('mustache');
+const fs = require('fs/promises');
+
+async function main() {
+
+  const payload = await generateData();
+  const template = await getTemplate();
+
+  const newReadMe = Mustache.render(template, payload);
+
+  await saveReadMe(newReadMe);
+}
+
+/**
+ * Example data object:
+ * {
+ *    posts: [
+ *      {
+ *        title: String,
+ *        link: String,
+ *        description: String,
+ *        date: Date
+ *      }
+ *    ],
+ *    videos: [
+ *      {
+ *        title: String,
+ *        link: String,
+ *        date: Date,
+ *        description: String,
+ *        thumbnail: String
+ *      }
+ *    ]
+ * }
+ */
+async function generateData() {
+
+  const videos = await _generateYTData();
+  const posts = await _generateBlogData();
+
+  return {
+    posts,
+    videos
+  };
+}
+
+async function _generateYTData() {
+  const parser = new Parser({
+    customFields: {
+      item: ['media:group', 'media:thumbnail'],
+    },
+  });
+
+  const feed = await parser.parseURL(
+    `https://www.youtube.com/feeds/videos.xml?channel_id=UCn2FoDbv_veJB_UbrF93_jw`
+  )
+
+  return feed.items.slice(0, 3).map((m) => {
+    return {
+      title: m.title,
+      link: m.link,
+      description: m['media:group']['media:description'][0],
+      thumbnail: m['media:group']['media:thumbnail'][0].$.url,
+      date: m.pubDate ? new Date(m.pubDate) : new Date()
+    }
+  });
+}
+
+async function _generateBlogData() {
+  const parser = new Parser();
+
+  const feed = await parser.parseURL('https://baldbeardedbuilder.com/feed.xml');
+
+  return feed.items.slice(0, 3).map((m) => {
+    return {
+      title: m.title,
+      link: m.link,
+      description: m.description,
+      date: m.pubDate ? new Date(m.pubDate) : new Date()
+    }
+  });
+}
+
+async function getTemplate() {
+  return await fs.readFile("_template.md", "utf-8")
+}
+
+async function saveReadMe(newReadMe) {
+
+  await fs.writeFile('../../README.md', newReadMe);
+}
+
+main();
