5c7f082e5731fc38be49aaaa5399bf05e342ccdb
Michael Jolley <mike@sparcapp.io>
2022-03-30 17:35:40 -0500

    Astro9

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 import tailwind from "@astrojs/tailwind";
-import astroImagePlugin from "astro-imagetools/plugin";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact(), tailwind()],
 	buildOptions: {
 		site: 'https://example.com/'
-	},
-	vite: {
-		plugins: [astroImagePlugin],
 	},
 });
