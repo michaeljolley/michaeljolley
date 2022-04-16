dac0b877a2f2ceb0bbeae78491d53e3660a223da
Michael Jolley <mike@sparcapp.io>
2022-04-16 15:21:18 -0500

    Preparing env on action runs

--- netlify/functions/smolify/index.js
+++ netlify/functions/smolify/index.js
@@ -1,82 +1,82 @@
 const Lokka = require('lokka').Lokka
 const Transport = require('lokka-transport-http').Transport
 
-const faunaSecret = process.env.FAUNADBSECRET
+const faunaSecret = process.env.VITE_FAUNADBSECRET
 
 const headers = {
-  Authorization: `Bearer ${faunaSecret}`,
+	Authorization: `Bearer ${faunaSecret}`,
 }
 const transport = new Transport('https://graphql.fauna.com/graphql', {
-  headers,
+	headers,
 })
 
 const client = new Lokka({
-  transport,
+	transport,
 })
 
 exports.handler = async (event, context) => {
-  let redirectUrl = 'https://baldbeardedbuilder.com/'
-  const path = event.queryStringParameters.path
+	let redirectUrl = 'https://baldbeardedbuilder.com/'
+	const path = event.queryStringParameters.path
 
-  const realdeal = await getLongUrl(path)
+	const realdeal = await getLongUrl(path)
 
-  if (realdeal && realdeal.url) {
-    redirectUrl = realdeal.url
-  }
+	if (realdeal && realdeal.url) {
+		redirectUrl = realdeal.url
+	}
 
-  return {
-    statusCode: 302,
-    headers: {
-      location: redirectUrl,
-      'Cache-Control': 'no-cache',
-    },
-    body: JSON.stringify({}),
-  }
+	return {
+		statusCode: 302,
+		headers: {
+			location: redirectUrl,
+			'Cache-Control': 'no-cache',
+		},
+		body: JSON.stringify({}),
+	}
 }
 
 const getLongUrl = async (path) => {
-  let longUrl
+	let longUrl
 
-  // Lookup path in FaunaDb & get the longUrl if it exists
-  try {
-    const variables = {
-      source: path,
-    }
+	// Lookup path in FaunaDb & get the longUrl if it exists
+	try {
+		const variables = {
+			source: path,
+		}
 
-    const data = await client.query(query, variables)
+		const data = await client.query(query, variables)
 
-    if (data.shortyMcShortLinkBySource && data.shortyMcShortLinkBySource) {
-      const shortLink = data.shortyMcShortLinkBySource
+		if (data.shortyMcShortLinkBySource && data.shortyMcShortLinkBySource) {
+			const shortLink = data.shortyMcShortLinkBySource
 
-      await recordVisit(shortLink)
+			await recordVisit(shortLink)
 
-      longUrl = shortLink.target
-    }
-  } catch (err) {
-    console.log(err)
-  }
+			longUrl = shortLink.target
+		}
+	} catch (err) {
+		console.log(err)
+	}
 
-  return {
-    url: longUrl,
-  }
+	return {
+		url: longUrl,
+	}
 }
 
 const recordVisit = async (shortLink) => {
-  // Save the updated visit count to Fauna
-  try {
-    const variables = {
-      id: shortLink._id,
-      data: {
-        source: shortLink.source,
-        target: shortLink.target,
-        visits: ++shortLink.visits,
-      },
-    }
+	// Save the updated visit count to Fauna
+	try {
+		const variables = {
+			id: shortLink._id,
+			data: {
+				source: shortLink.source,
+				target: shortLink.target,
+				visits: ++shortLink.visits,
+			},
+		}
 
-    await client.mutate(mutation, variables)
-  } catch (err) {
-    console.log(err)
-  }
+		await client.mutate(mutation, variables)
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 const query = `
