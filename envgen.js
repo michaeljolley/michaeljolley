75c04f87b6cc5f73721e669c50cb6ae946ad666f
Michael Jolley <mike@sparcapp.io>
2022-04-16 14:43:56 -0500

    Preparing env on action runs

--- envgen.js
+++ envgen.js
@@ -1,8 +1,8 @@
 const fs = require('fs/promises');
+require('dotenv').config();
 
 async function main() {
-	await fs.writeFile('.env', `
-TWITCH_CLIENT_SECRET=${process.env.TWITCH_CLIENT_SECRET}
+	await fs.writeFile('.env', `TWITCH_CLIENT_SECRET=${process.env.TWITCH_CLIENT_SECRET}
 TWITCH_CLIENT_ID=${process.env.TWITCH_CLIENT_ID}
 GOOGLE_ANALYTICS_ID=${process.env.GOOGLE_ANALYTICS_ID}
 CLOUDINARY_CLOUD_NAME=${process.env.CLOUDINARY_CLOUD_NAME}
