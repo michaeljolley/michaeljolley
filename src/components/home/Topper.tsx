26f7252d29b860f394c7d588f3896c9ff225c93c
Michael Jolley <mike@sparcapp.io>
2022-05-17 15:09:52 -0500

    I dunno

--- src/components/home/Topper.tsx
+++ src/components/home/Topper.tsx
@@ -10,30 +10,32 @@ function Topper() {
 				.then((json) => {
 					const { isOnline } = json;
 					isLive = isOnline;
+
+					return (
+						<section class="topper">
+							<div class={ isLive ? 'twitch intro' : 'intro' }>
+								{ isLive && (
+										<iframe
+											src="https://player.twitch.tv/?channel=baldbeardedbuilder&parent=baldbeardedbuilder.com&autoplay=true"
+											scrolling="no"
+											allow="autoplay"
+											allowfullscreen="false"
+										>
+										</iframe>
+									)
+								}
+							</div>
+						</section>
+					);
+
 				})
-				.catch((ex) => {});
+				.catch((ex) => console.log(ex));
 		})
-		.catch((ex) => { });
-	} catch (err) {
-
+		 .catch((ex) => console.log(ex));
 	}
-
-	return (
-		<section class="topper">
-			<div class={ isLive ? 'twitch intro' : 'intro' }>
-				{ isLive && (
-						<iframe
-							src="https://player.twitch.tv/?channel=baldbeardedbuilder&parent=baldbeardedbuilder.com&autoplay=true"
-							scrolling="no"
-							allow="autoplay"
-							allowfullscreen="false"
-						>
-						</iframe>
-					)
-				}
-			</div>
-		</section>
-	);
+	catch(ex) {
+		console.log(ex)
+	}
 }
 
 export default Topper;
