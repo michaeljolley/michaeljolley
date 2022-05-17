9014a6ce5cf5b18e19009053e5b066adfcacf2e1
Michael Jolley <mike@sparcapp.io>
2022-05-17 15:26:44 -0500

    Weeeeeeeee

--- src/components/home/Topper.tsx
+++ src/components/home/Topper.tsx
@@ -1,41 +1,38 @@
-function Topper() {
-	
-	let isLive = false;
+import { useEffect, useState} from 'preact/hooks';
 
-	try {
-		const response = fetch(
-			`/functions/twitch`
-		).then((response) => {
-			response.json()
+function Topper() {
+	const [isLive, setIsLive] = useState(false);
+
+  useEffect(() => {
+		fetch(
+				`/functions/twitch`
+			).then((response) => {
+				response.json()
 				.then((json) => {
 					const { isOnline } = json;
-					isLive = isOnline;
-
-					return (
-						<section class="topper">
-							<div class={ isLive ? 'twitch intro' : 'intro' }>
-								{ isLive && (
-										<iframe
-											src="https://player.twitch.tv/?channel=baldbeardedbuilder&parent=baldbeardedbuilder.com&autoplay=true"
-											scrolling="no"
-											allow="autoplay"
-											allowfullscreen="false"
-										>
-										</iframe>
-									)
-								}
-							</div>
-						</section>
-					);
-
+					setIsLive(isOnline);
 				})
 				.catch((ex) => console.log(ex));
-		})
-		 .catch((ex) => console.log(ex));
-	}
-	catch(ex) {
-		console.log(ex)
-	}
+			})
+		.catch((ex) => console.log(ex));
+  }, []);
+
+	return (
+		<section class="topper">
+			<div class={ isLive ? 'twitch intro' : 'intro' }>
+				{ isLive && (
+						<iframe
+							src="https://player.twitch.tv/?channel=baldbeardedbuilder&parent=baldbeardedbuilder.com&autoplay=true"
+							scrolling="no"
+							allow="autoplay"
+							allowfullscreen="false"
+						>
+						</iframe>
+					)
+				}
+			</div>
+		</section>
+	);
 }
 
 export default Topper;
