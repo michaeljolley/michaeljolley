deddfe0e71a1f31da7b9e2753f0600847b8772b9
Michael Jolley <mike@sparcapp.io>
2022-06-25 21:15:25 -0500

    Removing SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import netlify from '@astrojs/netlify/edge-functions';
 
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
 import remarkPrism from 'remark-prism';
 
 export default defineConfig({
-	adapter: netlify(),
 	integrations: [preact(), tailwind({
 		config: {
 			applyAstroPreset: false
