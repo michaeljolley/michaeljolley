e375b7f19c04fc721ac5f30c7435fa725a7c8751
Michael Jolley <mike@sparcapp.io>
2022-07-17 13:44:37 -0500

    Fixing compile

--- functions/smolify.js
+++ functions/smolify.js
@@ -2,68 +2,68 @@ import { Client, query } from 'faunadb';
 require('dotenv').config()
 
 const client = new Client({
-  secret: process.env.FAUNADBSECRET
+	secret: process.env.FAUNADBSECRET
 })
 
 exports.handler = async (event, context) => {
-  const shortUrl = await getLongUrl(event.queryStringParameters.path)
-  const redirectUrl = shortUrl ? shortUrl.target : 'https://baldbeardedbuilder.com/'
+	const shortUrl = await getLongUrl(event.queryStringParameters.path)
+	const redirectUrl = shortUrl ? shortUrl.target : 'https://baldbeardedbuilder.com/'
 
-  return {
-    statusCode: 302,
-    headers: {
-      location: redirectUrl,
-      'Cache-Control': 'no-cache',
-    },
-    body: JSON.stringify({}),
-  }
+	return {
+		statusCode: 302,
+		headers: {
+			location: redirectUrl,
+			'Cache-Control': 'no-cache',
+		},
+		body: JSON.stringify({}),
+	}
 }
 
-const getLongUrl = async (path)=> {
-  // Lookup path in FaunaDb & get the longUrl if it exists
-  try {
+const getLongUrl = async (path) => {
+	// Lookup path in FaunaDb & get the longUrl if it exists
+	try {
 
-    const response = await client.query < FaunaResponse > (
-      query.Map(
-        query.Paginate(
-          query.Match(
-            query.Index("shortyMcShortLinkBySource"), path
-          )
-        ),
-        query.Lambda("ShortyMcShortLink", query.Get((query.Var("ShortyMcShortLink"))))
-      )
-    )
+		const response = await client.query(
+			query.Map(
+				query.Paginate(
+					query.Match(
+						query.Index("shortyMcShortLinkBySource"), path
+					)
+				),
+				query.Lambda("ShortyMcShortLink", query.Get((query.Var("ShortyMcShortLink"))))
+			)
+		)
 
-    if (response.data && response.data.length > 0) {
-      const shortUrl = mapResponse < ShortUrl > (response.data[0])
-      await recordVisit(shortUrl)
-      return shortUrl
-    }
-  } catch (err) {
-    console.log(err)
-  }
+		if (response.data && response.data.length > 0) {
+			const shortUrl = mapResponse < ShortUrl > (response.data[0])
+			await recordVisit(shortUrl)
+			return shortUrl
+		}
+	} catch (err) {
+		console.log(err)
+	}
 
-  return undefined
+	return undefined
 }
 
 const recordVisit = async (shortUrl) => {
-  // Save the updated visit count to Fauna
-  try {
-    shortUrl.visits++
+	// Save the updated visit count to Fauna
+	try {
+		shortUrl.visits++
 
-    await client.query < FaunaDocument > (
-      query.Replace(query.Ref(query.Collection("ShortyMcShortLink"), shortUrl._id), {
-        data: shortUrl
-      })
-    )
-  } catch (err) {
-    console.log(err)
-  }
+		await client.query(
+			query.Replace(query.Ref(query.Collection("ShortyMcShortLink"), shortUrl._id), {
+				data: shortUrl
+			})
+		)
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 function mapResponse(payload) {
-  return {
-    ...payload.data,
-    _id: payload.ref.value.id
-  }
+	return {
+		...payload.data,
+		_id: payload.ref.value.id
+	}
 }
