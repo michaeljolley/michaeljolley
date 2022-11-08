7c2843d3d07c8ac0d24df3d3011cd568930322a2
Michael Jolley <mike@sparcapp.io>
2022-11-08 16:29:48 -0600

    SSR junk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import remarkGfm from 'remark-gfm';
 import rehypePrismPlus from 'rehype-prism-plus';
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
 	site: 'https://baldbeardedbuilder.com',
+	output: 'server',
 	integrations: [preact(), tailwind({
 		config: {
 			applyAstroPreset: false
@@ -17,17 +18,13 @@ export default defineConfig({
 		changefreq: 'weekly',
 		priority: 0.7,
 		lastmod: new Date(),
-		filter: (page) =>
-			page !== "https://baldbeardedbuilder.com/thanks"
+		filter: page => page !== "https://baldbeardedbuilder.com/thanks"
 	})],
 	trailingSlash: 'always',
 	markdown: {
-		rehypePlugins: [
-			rehypePrismPlus
-		],
-		remarkPlugins: [
-			remarkGfm
-		],
+		rehypePlugins: [rehypePrismPlus],
+		remarkPlugins: [remarkGfm],
 		syntaxHighlight: 'prism'
-	}
+	},
+	adapter: netlify()
 });
