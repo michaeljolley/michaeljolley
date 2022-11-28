b649005e3f6174e635bcaacd01519b3d851cc9a7
Michael Jolley <mike@sparcapp.io>
2022-11-27 22:07:09 -0600

    Fixing numerous things

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,6 @@ export const get = () =>
     description:
       "Home of Michael Jolley, the bald bearded builder, this site contains a collection of all blog posts, YouTube videos, Twitch streams, conference talks and more that Michael has generated.",
     site: import.meta.env.SITE,
-    items: import.meta.glob("./**/*.mdx"),
+    items: import.meta.glob("./**/*.md"),
     customData: `<language>en-us</language>`,
   });
